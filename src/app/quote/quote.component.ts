590d94b026c7d0c00576f4a1088a5186a3933ab9
peterkush <pkungu004@gmail.com>
2020-11-11 05:58:13 +0300

    modified quote componenent.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,13 +9,9 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
     Quotes = [
-      new Quote(1, 'A.B .Peter', '"Success is doing what you want to do, when you want, where you want, with whom you want, as much as you want."', new Date(2020, 10, 8) ),
+      new Quote(1, 'A.B .Peter', '"The Sun is Also a Star🤔."', new Date(2020, 10, 8) ),
       new Quote(2, 'B.C .Abdul', '“i love Earth.”', new Date(2020, 10, 8) ),
-      // new Quote(3, '-Steve Jobs', “If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.”', new Date(2020, 11, 8) ),
-      // // new Quote(4, '-Rob Siltanen', '“People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.”', new Date(2018, 8, 15) ),
-      // // new Quote(5, '-Eleanor Roosevelt', '"The future belongs to those who believe in the beauty of their dreams."', new Date(2018, 8, 15) ),
-      // // new Quote(6, '-Don Zimmer', '“What You Lack In Talent Can Be Made Up With Desire, Hustle And Giving 110% All The Time.”', new Date(2018, 8, 15) ),
-    ]
+  
 
 
     toogleDetails(index){
